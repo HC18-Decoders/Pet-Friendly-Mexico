9b26c35f7946006dd3f20190bfbeff0fa4f2c5ed
luisgnava <luis.gnava85@gmail.com>
2018-03-20 19:15:51 -0600

    done half of the readme left the task to cesar

---
+++ schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS profiles;
+
+CREATE DATABASE profiles;
+
+use profiles;
+
+CREATE TABLE vetProfile(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  phone_num INT NOT NULL,
+  address TEXT
+);
+
+CREATE TABLE petProfile(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name_pet varchar(255) NOT NULL,
+  age INT NOT NULL,
+  breed varchar(255) NOT NULL,
+  vaccines varchar(255) NOT NULL,
+  wormed_out BIT NOT NULL,
+  short_description TEXT NOT NULL
+);
