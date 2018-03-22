1b93c71b37831a31cb6becc9fcc7b1ec3a28155f
JorgeC02018 <jorgecc0218@gmail.com>
2018-03-22 17:11:09 -0600

    changes made to CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Header from './home/Header.jsx'
