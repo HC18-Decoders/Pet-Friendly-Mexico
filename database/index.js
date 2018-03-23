dcb15c56385d27edb5c4b6426531e1f9fa8572db
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-03-23 17:30:58 -0600

    update function name from petProfile to vetProfile

--- database/index.js
+++ database/index.js
@@ -3,9 +3,9 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const getPetProfile = function() {
+const vetPetProfile = function() {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM petProfile', (err, data) => {
+    connection.query('SELECT * FROM vetProfile', (err, data) => {
       if(err) {
         return reject(err);
       }
