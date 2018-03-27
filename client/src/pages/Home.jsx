1265a91f15d1608b77bf642d5d0ebb26858dc784
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-03-27 11:49:51 -0600

    created a Home component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import Header from './home/Header.jsx';
+import Search from './home/Search.jsx';
+
+
+
+
+
+
+
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Search />
+        </div>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <footer className="foo">Hi! What are you looking for?</footer>
+        </div>
+      </div>
+    );
+  }
+}
