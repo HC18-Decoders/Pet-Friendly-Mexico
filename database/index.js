1621dc174f2c7960653ff40afe4a1232f427c94d
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-03-27 19:13:43 -0600

    created getSignUp function

--- database/index.js
+++ database/index.js
@@ -3,6 +3,17 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
+const getSignUp = function() {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM signUp', (err, data) => {
+      if(err) {
+        return reject(err);
+      }
+      return resolve(data);
+    })
+  })
+};
+
 const getVetProfile = function() {
   return new Promise((resolve, reject) => {
     connection.query('SELECT * FROM vetProfile', (err, data) => {
