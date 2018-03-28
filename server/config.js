2f6b2eea6837e9316ae943478a9ed1b86247c060
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-03-28 16:03:43 -0600

    commented home route

--- server/config.js
+++ server/config.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/', route.)
+//app.get('/', route)
 
 app.get('/signUp', route.getSignUp)
 
