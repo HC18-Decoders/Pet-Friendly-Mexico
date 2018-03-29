1d5731126c9394abada795287611113e17cb164a
luisgnava <luis.gnava85@gmail.com>
2018-03-28 18:53:27 -0600

    first commit for 20-toolbar

--- client/src/home/Header.jsx
+++ client/src/home/Header.jsx
@@ -25,19 +25,8 @@ export default class Header extends React.Component {
 render() {
  return(
   <Toolbar>
-          <ToolbarGroup firstChild={true}>
-            <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="All Broadcasts" />
-              <MenuItem value={2} primaryText="All Voice" />
-              <MenuItem value={3} primaryText="All Text" />
-              <MenuItem value={4} primaryText="Complete Voice" />
-              <MenuItem value={5} primaryText="Complete Text" />
-              <MenuItem value={6} primaryText="Active Voice" />
-              <MenuItem value={7} primaryText="Active Text" />
-            </DropDownMenu>
-          </ToolbarGroup>
           <ToolbarGroup>
-            <ToolbarTitle text="Options" />
+            <ToolbarTitle text="Pet's Mexico" />
             <FontIcon className="muidocs-icon-custom-sort" />
             <ToolbarSeparator />
             <RaisedButton label="Create Broadcast" primary={true} />
