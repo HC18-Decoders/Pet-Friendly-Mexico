b254fb3a6976c8ee201a0ba243f0331461107d6d
luisgnava <luis.gnava85@gmail.com>
2018-03-28 19:04:10 -0600

    removed minor useless feats on toolbar

--- client/src/home/Header.jsx
+++ client/src/home/Header.jsx
@@ -4,10 +4,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import {darkBlack, white} from 'material-ui/styles/colors';
@@ -27,19 +23,7 @@ render() {
   <Toolbar>
           <ToolbarGroup>
             <ToolbarTitle text="Pet's Mexico" />
-            <FontIcon className="muidocs-icon-custom-sort" />
             <ToolbarSeparator />
-            <RaisedButton label="Create Broadcast" primary={true} />
-            <IconMenu
-              iconButtonElement={
-                <IconButton touch={true}>
-                  <NavigationExpandMoreIcon />
-                </IconButton>
-              }
-            >
-              <MenuItem primaryText="Download" />
-              <MenuItem primaryText="More Info" />
-            </IconMenu>
           </ToolbarGroup>
         </Toolbar>
   );
--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: 'localhost',
   user: 'root',
-  password: 'holacode',
+  password: 'Holacode',
   database: ''
 };
