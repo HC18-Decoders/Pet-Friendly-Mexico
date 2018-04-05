35d5d87b17715f0624f8230f5c08aaad4bca632c
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-04-05 15:25:52 -0500

    modified Header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import Header from '../home/Header.jsx';
 import Search from '../home/Search.jsx';
-
-
+import Header from '../home/Header.jsx';
 
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+
+    }
   }
 
 
  render() {
    return (
-
      <div>
        <div>
          <Header />
