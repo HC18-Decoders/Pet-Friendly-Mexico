87a73d19c3b909ea2af744f021abc7bcaf874ff2
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-04-07 17:01:57 -0500

    added routes for service and pet components

--- client/src/PrimaryLayout.jsx
+++ client/src/PrimaryLayout.jsx
@@ -4,6 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 import Home from "./pages/Home.jsx"
 //import Header from "./home/Header.jsx"
 import SearchResults from "./pages/SearchResults.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import ServicesProfile from "./pages/ServicesProfile.jsx";
+
+
 
 const PrimaryLayout = ({ match }) => (
   <div className="primary-layout">
@@ -11,6 +15,8 @@ const PrimaryLayout = ({ match }) => (
       <Switch>
         <Route exact path={`${match.path}`} component={Home} />
         <Route path={`${match.url}searchresults`} component={SearchResults} />
+        <Route path={`${match.path}userprofile`} component={UserProfile} />
+        <Route path={`${match.path}servicesprofile`} component={ServicesProfile} />
       </Switch>
     </main>
   </div>
