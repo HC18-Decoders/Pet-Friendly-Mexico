6c9c500f856c1830552286f3fe26a82da8cc7a39
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-04-07 17:02:27 -0500

    deleted unwated components

--- client/src/components/PetList.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import VetThumbnail from "./PetThumbnail.jsx";
-
-const PetList = (props) => (
-  <div>
-    {props.pets.map(pet => <PetThumbnail pet={pet} key={pet.id} />)}
-  </div>
-)
