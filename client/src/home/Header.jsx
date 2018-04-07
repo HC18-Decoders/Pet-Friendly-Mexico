e4c8332c03f52b52f6e6f1def29ac3b254cbfd2b
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-04-06 20:13:46 -0500

    added the menu button

--- client/src/home/Header.jsx
+++ client/src/home/Header.jsx
@@ -7,15 +7,20 @@ import {darkBlack, white} from 'material-ui/styles/colors';
 import firebase from 'firebase';
 import {provider, auth} from '../config/client.js';
 import runtimeGenerator from 'regenerator-runtime/runtime';
-
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Header extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      open: false
     }
+    this.handleToggle=this.handleToggle.bind(this)
+    this.handleClose=this.handleClose.bind(this)
     this.login=this.login.bind(this)
     this.logout=this.logout.bind(this)
   }
@@ -40,10 +45,25 @@ export default class Header extends React.Component {
     alert('This button was clicked');
   }
 
+  handleToggle() {
+    this.setState({open: !this.state.open});
+}
+
+ handleClose(){
+   this.steState({open: false});
+}
+
   render() {
     return(
       <Toolbar className= "toolBar" style={{backgroundColor:darkBlack}}>
           <ToolbarGroup>
+            <div>
+              <FlatButton label='Menu' onClick={this.handleToggle} style={{color: white}}/>
+              <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+              <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+              <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+            </Drawer>
+            </div>
             <ToolbarTitle text="Pet's Mexico" style={{color:white}}/>
             <ToolbarSeparator />
             <FlatButton className="login" onClick={this.login} label="Login with Facebook" style={{right: -805, color: white}} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,22 +6,18 @@ import Services from './ServicesProfiles.jsx';
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
 
  render() {
    return (
-
      <div>
        <div>
          <Header />
        </div>
        <div>
          <Search />
-       </div>
-       <div>
-         <Services />
        </div>
        <div>
          <footer className="foo">¡Hola! ¿Qué estás buscando?</footer>
