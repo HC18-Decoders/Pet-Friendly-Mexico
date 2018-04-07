944a14ff19f4f8e769a3183b0721f458b97f8d64
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-04-07 17:05:18 -0500

    created a pet profile component

---
+++ client/src/components/user/AddPetProfile.jsx
@@ -0,0 +1,145 @@
+import React from 'react';
+
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import {darkBlack, white} from 'material-ui/styles/colors';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+
+class AddPetProfile extends React.Component{
+ constructor(props){
+   super(props);
+   this.state = {
+     petName: '',
+     age: '',
+     breed: '',
+     vaccines: '',
+     dewormed: '',
+     smallDescription: ''
+  }
+  this.updatePetName = this.updatePetName.bind(this);
+  this.addNewProfile = this.addNewProfile.bind(this);
+ }
+
+ updatePetName(event){
+   this.setState({
+     petName: event.target.value
+   })
+ }
+
+ updateAge(event){
+   this.setState({
+     age: event.target.value
+   })
+ }
+
+ updateBreed(event){
+   this.setState({
+     breed: event.target.value
+   })
+ }
+
+ updateVaccines(event){
+   this.setState({
+     vaccines: event.target.value
+   })
+ }
+
+ updateDewormed(event){
+   this.setState({
+     dewormed: event.target.value
+   })
+ }
+
+ updateSmallDescription(event){
+   this.setState({
+     smallDescription: event.target.value
+   })
+ }
+
+
+
+  addNewProfile(){
+    this.props.insertPetProfile(this.state.petName, this.state.age,
+       this.state.breed, this.state.vaccines, this.state.dewormed, this.state.smallDescription);
+    this.setState({
+      petName: '',
+      age: '',
+      breed: '',
+      vaccines: '',
+      dewormed: '',
+      smallDescription: ''
+    })
+  }
+
+ render() {
+    return (
+      <div>
+        <div>
+          <Avatar
+          icon={<FontIcon className="muidocs-icon-communication-voicemail" />}
+          size={250}
+          />
+        </div>
+        <div>
+          <TextField title="petName"
+                     onChange={this.updatePetName}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Nombre de Mascota" />
+        </div>
+        <div>
+          <TextField title="age"
+                     onChange={this.updateAge}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Edad"  />
+        </div>
+        <div>
+          <TextField title="breed"
+                     onChange={this.updateBreed}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Raza"
+                     colors="darkBlack" />
+        </div>
+        <div>
+          <TextField title="vaccines"
+                     onChange={this.updateVaccines}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Vacunas" />
+        </div>
+        <div>
+          <TextField title="dewormed"
+                     onChange={this.updateDewormed}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Desparasitado(a)?" />
+        </div>
+        <div>
+          <TextField title="smallDescription"
+                     onChange={this.updateSmallDescription}
+                     underlineStyle="white"
+                     fullLength={true}
+                     style={{color:darkBlack}}
+                     floatingLabelText="Descripción" />
+        </div>
+        <div>
+          <FlatButton className="submitProf"
+                      label="Submit"
+                      onClick={this.addNewProfile}
+                      style={{left: 680, color: white}} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddPetProfile;
